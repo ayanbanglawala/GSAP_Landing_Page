73136443fb7459b204f8bf63ffe5125e6dc335c1
Ayan Banglawala <ayanchhipa2278@gmail.com>
2025-07-28 11:18:36 +0530

    aa

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -6,8 +6,23 @@ import { featureLists, goodLists } from '../constants'
 const Art = () => {
  const isMobile = useMediaQuery({ maxWidth: 767 });
  
- useGSAP(()=>{
-    const start = isMobile ? 'top 20%' : 'top top'
+ useGSAP(() => {
+	const start = isMobile ? 'top 20%' : 'top top';
+	
+	const maskTimeline = gsap.timeline({
+	 scrollTrigger: {
+		trigger: '#art',
+		start,
+		end: 'bottom center',
+		scrub: 1.5,
+		pin: true
+	 }
+	})
+	
+	maskTimeline
+	 .to('.will-fade', { opacity: 0, stagger: 0.2, ease: 'power1.inOut', })
+	 .to('.masked-img', { scale: 1.3, maskPosition: 'center', maskSize: '400%', duration: 1, ease: 'power1.inOut '})
+	 .to('#masked-content', { opacity: 1, duration: 1, ease: 'power1.inOut'})
  })
  
  return (
